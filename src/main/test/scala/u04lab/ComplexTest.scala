2c88aaf64c8311bf4e38e50a48814b7cd4bbb0eb
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-21 11:14:02 +0100

    Add test to check equals and tostring not working

--- src/main/test/scala/u04lab/ComplexTest.scala
+++ src/main/test/scala/u04lab/ComplexTest.scala
@@ -7,3 +7,12 @@ import u04lab.code.Complex
 class ComplexTest:
 
   import Complex.*
+
+  @Test def checkEquals(): Unit =
+    val c1 = Complex(10.0, 3.0)
+    val c2 = Complex(10.0, 3.0)
+    assertEquals(c1, c2)
+
+  @Test def checkToString(): Unit =
+    val c = Complex(10.0, 3.0)
+    assertEquals(c.toString, "ComplexImpl(10.0,3.0)")
